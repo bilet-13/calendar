b74a70a2517e18602a944a97b61d3d016a94ae5e
bilet-13 <liao0616237.cs06@edu.tw>
2019-05-06 21:15:43 +0800

    modify style.scss

--- src/pages/Calander/bilet.js
+++ src/pages/Calander/bilet.js
@@ -5,74 +5,74 @@ class bilet extends React.Component {
   render () {
     return (
       <div>
-      <Layout>
-        <div className={style.space} />
-        <div className={style.box}>
-          <div className={style.title}>
-            <div className={style.date}>4/12</div>
-            <div className={style.btnblack}>X</div>
-          </div>
-          <div className={style.item}>
-            <div className={style.tagblue} />
-            <div className={style.content}>
-              <div>
+        <Layout>
+          <div className={style.space} />
+          <div className={style.box}>
+            <div className={style.title}>
+              <div className={style.date}>4/12</div>
+              <div className={style.btnblack}>X</div>
+            </div>
+            <div className={style.item}>
+              <div className={style.tagblue} />
+              <div className={style.content}>
+                <div>
               日文(三)    作業
-              </div>
-              <div >
+                </div>
+                <div >
               不想寫作業啦啦啦啦啦啦啦啦啦啦啦啦
+                </div>
+              </div>
+              <div className={style.del}>
+                <i className={`fa fa-trash fa-lg ${style.shad}`} />
               </div>
             </div>
-            <div className={style.del}>
-              <i className={`fa fa-trash fa-lg ${style.shad}`} />
-            </div>
-          </div>
-          <div className={style.item}>
-            <div className={style.tagyellow} />
-            <div className={style.content}>
-              <div>
+            <div className={style.item}>
+              <div className={style.tagyellow} />
+              <div className={style.content}>
+                <div>
               正規語言概論    小考
-              </div>
-              <div >
+                </div>
+                <div >
               太可怕啦
+                </div>
+              </div>
+              <div className={style.del}>
+                <i className={`fa fa-trash fa-lg ${style.shad}`} />
               </div>
             </div>
-            <div className={style.del}>
-              <i className={`fa fa-trash fa-lg ${style.shad}`} />
-            </div>
-          </div>
-          <div className={style.item}>
-            <div className={style.tagred} />
-            <div className={style.content}>
-              <div>
+            <div className={style.item}>
+              <div className={style.tagred} />
+              <div className={style.content}>
+                <div>
               通識  報告
-              </div>
-              <div >
+                </div>
+                <div >
               還趕下來啊冰鳥
+                </div>
+              </div>
+              <div className={style.del}>
+                <i className={`fa fa-trash fa-lg ${style.shad}`} />
               </div>
             </div>
-            <div className={style.del}>
-              <i className={`fa fa-trash fa-lg ${style.shad}`} />
-            </div>
-          </div>
-          <div className={style.item}>
-            <div className={style.tagpink} />
-            <div className={style.content}>
-              <div>
+            <div className={style.item}>
+              <div className={style.tagpink} />
+              <div className={style.content}>
+                <div>
               籃球梅竹賽
-              </div>
-              <div >
+                </div>
+                <div >
               看籃球囉
+                </div>
+              </div>
+              <div className={style.del}>
+                <i className={`fa fa-trash fa-lg ${style.shad}`} />
               </div>
             </div>
-            <div className={style.del}>
-              <i className={`fa fa-trash fa-lg ${style.shad}`} />
+            <div className={style.create}>
+              <i className={`fa fa-plus-circle fa-lg ${style.shad}`} />
             </div>
           </div>
-          <div className={style.create}>
-            <i className={`fa fa-plus-circle fa-lg ${style.shad}`} />
-          </div>
-        </div>
-      </Layout>
+        </Layout>
       </div>
     )
   }
